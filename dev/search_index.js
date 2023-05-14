7cb2943884bb3ee652288098e36d21e307df44f1
Documenter.jl <documenter@juliadocs.github.io>
2023-05-14 03:21:57 +0000

    build based on 06d8ad7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NaturalNeighbourInterp","category":"page"},{"location":"#NaturalNeighbourInterp","page":"Home","title":"NaturalNeighbourInterp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NaturalNeighbourInterp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NaturalNeighbourInterp]","category":"page"}]
+}
